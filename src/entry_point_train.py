c5f7f4d1c437fbc8cbc981ba827127cdf79ee580
Peter Siemen <peter.siemen@googlemail.com>
2019-06-17 10:45:39 +0200

    model_fn in entry_point_tain.py

--- src/entry_point_train.py
+++ src/entry_point_train.py
@@ -11,36 +11,36 @@ import torch.utils.data
 
 from train.model import LSTMClassifier
 
-# def model_fn(model_dir):
-#     """Load the PyTorch model from the `model_dir` directory."""
-#     print("Loading model.")
-#
-#     # First, load the parameters used to create the model.
-#     model_info = {}
-#     model_info_path = os.path.join(model_dir, 'model_info.pth')
-#     with open(model_info_path, 'rb') as f:
-#         model_info = torch.load(f)
-#
-#     print("model_info: {}".format(model_info))
-#
-#     # Determine the device and construct the model.
-#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     model = LSTMClassifier(model_info['embedding_dim'], model_info['hidden_dim'], model_info['vocab_size'])
-#
-#     # Load the stored model parameters.
-#     model_path = os.path.join(model_dir, 'model.pth')
-#     with open(model_path, 'rb') as f:
-#         model.load_state_dict(torch.load(f))
-#
-#     # Load the saved word_dict.
-#     word_dict_path = os.path.join(model_dir, 'word_dict.pkl')
-#     with open(word_dict_path, 'rb') as f:
-#         model.word_dict = pickle.load(f)
-#
-#     model.to(device).eval()
-#
-#     print("Done loading model.")
-#     return model
+def model_fn(model_dir):
+    """Load the PyTorch model from the `model_dir` directory."""
+    print("Loading model.")
+
+    # First, load the parameters used to create the model.
+    model_info = {}
+    model_info_path = os.path.join(model_dir, 'model_info.pth')
+    with open(model_info_path, 'rb') as f:
+        model_info = torch.load(f)
+
+    print("model_info: {}".format(model_info))
+
+    # Determine the device and construct the model.
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = LSTMClassifier(model_info['embedding_dim'], model_info['hidden_dim'], model_info['vocab_size'])
+
+    # Load the stored model parameters.
+    model_path = os.path.join(model_dir, 'model.pth')
+    with open(model_path, 'rb') as f:
+        model.load_state_dict(torch.load(f))
+
+    # Load the saved word_dict.
+    word_dict_path = os.path.join(model_dir, 'word_dict.pkl')
+    with open(word_dict_path, 'rb') as f:
+        model.word_dict = pickle.load(f)
+
+    model.to(device).eval()
+
+    print("Done loading model.")
+    return model
 
 def _get_train_data_loader(batch_size, training_dir):
     print("Get train data loader.")
